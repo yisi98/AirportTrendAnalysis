23d815bc30e8874ce7d998905a5acaa4d2967444
AirportTrendAnalysis <yisi98@hotmail.com>
2024-04-20 12:59:19 +0100

    introduced checkpoint saving and skipped already completed parts of the dataframe to save cost if process broke.

--- src/location_weather_llm.py
+++ src/location_weather_llm.py
@@ -2,13 +2,16 @@
 This file will use LLM to obtain the location's weather information
 It will include prompting LLM API and formatting the response in a way to interpret into airport dataframe
 """
+
 import json
+import os
 
 import numpy as np
 import pandas as pd
 from chronological import read_prompt, cleaned_completion
 from loguru import logger
 from openai import OpenAI
+from tqdm import tqdm
 
 
 def check_not_present_or_unsure(string):
@@ -28,47 +31,94 @@ def obtain_location_data(location: str, country: str):
         model="gpt-3.5-turbo-0125",
         response_format={"type": "json_object"},
         messages=[
-            {"role": "system", "content": "You are a helpful assistant designed to output JSON. If you cannot find "
-                                          "the value, put 'NA' and do not make up your answers. Example output should "
-                                          "look like:: average_temp : 25, average_wind_speed_annual: 12, "
-                                          "average_humidity_annual:  72, average_visibility_annual: 22,"},
-            {"role": "user", "content": f"What is the average temperature throughout the year and give just a value "
-                                        f"in Celcius, strictly not a range.  What is the average wind speed of that "
-                                        f"city, provide one value in knots. What is the average percentage of "
-                                        f"humidity in that city in a year, just provide the value without percentage "
-                                        f"signs. What is the average visibility in that city in the year, "
-                                        f"in km? Provide answer without additional text and strictly only the JSON "
-                                        f"format below"}
-        ]
+            {
+                "role": "system",
+                "content": "You are a helpful assistant designed to output JSON. If you cannot find "
+                "the value, put 'NA' and do not make up your answers. Example output should "
+                "look like:: average_temp : 25, average_wind_speed_annual: 12, "
+                "average_humidity_annual:  72, average_visibility_annual: 22,",
+            },
+            {
+                "role": "user",
+                "content": f"What is the average temperature in {location} {country} throughout the year "
+                f"and give just a value"
+                f"in Celcius, strictly not a range.  What is the average wind speed of {location} {country}"
+                f", provide one value in knots. What is the average percentage of "
+                f"humidity in {location} {country} in a year, just provide the value without "
+                f"percentage"
+                f"signs. What is the average visibility in {location} {country} in the year, "
+                f"in km? Provide answer without additional text and strictly only the JSON "
+                f"format below",
+            },
+        ],
     )
-
-    data = json.loads(response.choices[0].message.content)
-    avg_temperature = data['average_temp']
-    avg_wind_speed = data['average_wind_speed_annual']
-    avg_humidity = data['average_humidity_annual']
-    avg_visibility = data['average_visibility_annual']
-    return avg_temperature, avg_wind_speed, avg_humidity, avg_visibility
+    try:
+        data = json.loads(response.choices[0].message.content)
+        avg_temperature = data.get("average_temp", "NA")
+        avg_wind_speed = data.get("average_wind_speed_annual", "NA")
+        avg_humidity = data.get("average_humidity_annual", "NA")
+        avg_visibility = data.get("average_visibility_annual", "NA")
+        return (
+            str(avg_temperature),
+            str(avg_wind_speed),
+            str(avg_humidity),
+            str(avg_visibility),
+        )
+    except json.JSONDecodeError as e:
+        print(f"Failed to decode JSON: {e}")
+        return e, None, None, None
 
 
 def run_llm_queries(df):
-    # function which uses GPT to obtain datapoints such as weather
-    # create new columns
-    df["avg_temperature"] = np.nan
-    df["avg_wind_speed"] = np.nan
-    df["avg_humidity"] = np.nan
-    df["avg_visibility"] = np.nan
-    # run the queries
-    for index, row in df.iterrows():
-        avg_temperature, avg_wind_speed, avg_humidity, avg_visibility = obtain_location_data(row["original_city"],
-                                                                                             row["country"])
-        df.loc[index, "avg_temperature"] = avg_temperature
-        df.loc[index, "avg_wind_speed"] = avg_wind_speed
-        df.loc[index, "avg_humidity"] = avg_humidity
-        df.loc[index, "avg_visibility"] = avg_visibility
-        logger.info(f"country: {row['country']}, city: {row['original_city']}, avg_temperature: {avg_temperature}, "
-                    f"avg_wind_speed: {avg_wind_speed}, avg_humidity: {avg_humidity}, avg_visibility: {avg_visibility}")
+    checkpoint_folder = "../data/saved_checkpoints"
+    os.makedirs(checkpoint_folder, exist_ok=True)
 
-    return df
+    checkpoint_size = 100
+    total_rows = len(df)
+    all_dfs = (
+        []
+    )  # List to store dataframes from each checkpoint for final concatenation
+
+    # Wrap range with tqdm for a progress bar
+    for start in tqdm(
+        range(0, total_rows, checkpoint_size), desc="Processing checkpoints"
+    ):
+        end = min(start + checkpoint_size, total_rows)
+        checkpoint_path = (
+            f"{checkpoint_folder}/checkpoint_{start // checkpoint_size}.csv"
+        )
+
+        if os.path.exists(checkpoint_path):
+            logger.info(
+                f"Skipping processing for {checkpoint_path} as it already exists."
+            )
+            checkpoint_df = pd.read_csv(checkpoint_path)
+        else:
+            checkpoint_df = df[start:end].copy()
+            for index, row in tqdm(
+                checkpoint_df.iterrows(),
+                total=len(checkpoint_df),
+                desc=f"Checkpoint {start // checkpoint_size}",
+            ):
+                avg_temperature, avg_wind_speed, avg_humidity, avg_visibility = (
+                    obtain_location_data(row["original_city"], row["country"])
+                )
+                checkpoint_df.at[index, "avg_temperature"] = avg_temperature
+                checkpoint_df.at[index, "avg_wind_speed"] = avg_wind_speed
+                checkpoint_df.at[index, "avg_humidity"] = avg_humidity
+                checkpoint_df.at[index, "avg_visibility"] = avg_visibility
+                logger.info(
+                    f"Processed {row['country']}, {row['original_city']} - Checkpoint {start // checkpoint_size} - {avg_temperature}"
+                )
+
+            checkpoint_df.to_csv(checkpoint_path, index=False)
+            logger.info(f"Saved checkpoint at {checkpoint_path}")
+
+        all_dfs.append(checkpoint_df)
+
+    # Concatenate all dataframes to form the final dataframe
+    final_df = pd.concat(all_dfs, ignore_index=True)
+    return final_df
 
 
 def main():
